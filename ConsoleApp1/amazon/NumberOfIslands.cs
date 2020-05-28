6b742f518c77771cef4706cd6a180fc00725b9a6
maxeemus <maxeemus@gmail.com>
2020-05-28 19:07:06 -0400

    First DFS

--- ConsoleApp1/amazon/NumberOfIslands.cs
+++ ConsoleApp1/amazon/NumberOfIslands.cs
@@ -10,10 +10,19 @@ namespace ConsoleApp1.Amazon
         {
             var num = NumIslands(new char[][] 
             {
-                "11110".ToCharArray(),
-                "11010".ToCharArray(),
                 "11000".ToCharArray(),
-                "00010".ToCharArray(),
+                "11000".ToCharArray(),
+                "00100".ToCharArray(),
+                "00011".ToCharArray(),
+            });
+
+            num = NumIslands(new char[][] 
+            {
+                "111111".ToCharArray(),
+                "100001".ToCharArray(),
+                "101101".ToCharArray(),
+                "100001".ToCharArray(),
+                "111111".ToCharArray(),
             });
         }
 
@@ -32,13 +41,13 @@ namespace ConsoleApp1.Amazon
             grid[row][col] == '1' && !visited.Contains((row, col));
 
         private static void Dfs(char[][] grid, int row, int col, HashSet<(int, int)> visited)
-        {
+        {                        
             visited.Add((row, col)); // mark as visited to prevent inf. traversal
             foreach(var (r, c) in cellNbrs.Select(n => (r: row + n[0], c: col + n[1]))) // iterate all neibours 
             {
-                if(CanTraverseCell(grid, r, c, visited))
+                if(CanTraverseCell(grid, r, c, visited))                
                     Dfs(grid, r, c, visited);
-            }
+            }            
         }
 
         public int NumIslands(char[][] grid) 
@@ -51,7 +60,7 @@ namespace ConsoleApp1.Amazon
                 {
                     if(CanTraverseCell(grid, row, col, visited))
                     {
-                        Dfs(grid, row, col, visited);
+                        Dfs(grid, row, col, visited);                        
                         islandCount++;
                     }
 
