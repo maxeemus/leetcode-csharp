71374fbccaed1a8798e4c4716ddf72dd934baaee
maxeemus <maxeemus@gmail.com>
2020-07-16 17:21:56 -0400

    better performance solution

--- ConsoleApp1/SwapMaxRepeatedCharSubstring.cs
+++ ConsoleApp1/SwapMaxRepeatedCharSubstring.cs
@@ -12,22 +12,22 @@ namespace ConsoleApp1
     {
         public static void Test()
         {
-            new SwapMaxRepeatedCharSubstring().MaxLength("aabacbbbbbb").Should().Be(7);
-            new SwapMaxRepeatedCharSubstring().MaxLength("acaa").Should().Be(3);
-            new SwapMaxRepeatedCharSubstring().MaxLength("aabaaabaaaba").Should().Be(7);
-            new SwapMaxRepeatedCharSubstring().MaxLength("bbababaaaa").Should().Be(6);
-            new SwapMaxRepeatedCharSubstring().MaxLength("abacda").Should().Be(3);
-            new SwapMaxRepeatedCharSubstring().MaxLength("").Should().Be(0);
-            new SwapMaxRepeatedCharSubstring().MaxLength("a").Should().Be(1);
-            new SwapMaxRepeatedCharSubstring().MaxLength("ab").Should().Be(1);
-            new SwapMaxRepeatedCharSubstring().MaxLength("abb").Should().Be(2);
-            new SwapMaxRepeatedCharSubstring().MaxLength("abcdef").Should().Be(1);
-            new SwapMaxRepeatedCharSubstring().MaxLength("aba").Should().Be(2);
-            new SwapMaxRepeatedCharSubstring().MaxLength("aabac").Should().Be(3);
-            new SwapMaxRepeatedCharSubstring().MaxLength("ababa").Should().Be(3);
-            new SwapMaxRepeatedCharSubstring().MaxLength("aaabaaa").Should().Be(6);
-            new SwapMaxRepeatedCharSubstring().MaxLength("aaabbaaa").Should().Be(4);
-            new SwapMaxRepeatedCharSubstring().MaxLength("aaaaa").Should().Be(5);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("aabacbbbbbb").Should().Be(7);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("acaa").Should().Be(3);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("aabaaabaaaba").Should().Be(7);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("bbababaaaa").Should().Be(6);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("abacda").Should().Be(3);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("").Should().Be(0);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("a").Should().Be(1);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("ab").Should().Be(1);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("abb").Should().Be(2);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("abcdef").Should().Be(1);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("aba").Should().Be(2);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("aabac").Should().Be(3);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("ababa").Should().Be(3);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("aaabaaa").Should().Be(6);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("aaabbaaa").Should().Be(4);
+            new SwapMaxRepeatedCharSubstring().MaxLength2("aaaaa").Should().Be(5);
         }
 
         public int MaxLength(string text)
@@ -124,5 +124,56 @@ namespace ConsoleApp1
 
             return max;
         }
+
+        // better performance
+        public int MaxLength2(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return 0;
+            
+            // get same char groups: aaabaaa -> {"a", 3}, {"b", 1}, {"c", 3}
+            var charGroups = new List<(char c, int l)>();
+            char curC = '\0';
+            int li = 0;            
+            foreach(var c in text)
+            {
+                if(curC != c)
+                {                    
+                    curC = c;
+                    charGroups.Add((curC, 0));
+                    li = charGroups.Count - 1;                    
+                }
+                charGroups[li] = (charGroups[li].c, charGroups[li].l + 1);
+            }
+            
+            // get chars total count: aaabaaa -> {"a", 6}, {"b", 1}
+            var charCounts = new Dictionary<char, int>();
+            foreach(var c in text)
+            {
+                if(!charCounts.ContainsKey(c))
+                    charCounts.Add(c, 0);
+                charCounts[c]++;
+            }
+            
+            int max = 0;
+            // for each group get min of group length + 1 and total symbol for group
+            // and then max of all such minimums
+            // aaabaaa -> {"a", 3}, {"b", 1}, {"c", 3} / {"a", 6}, {"b", 1}
+            // for 'a' min(3 + 1, 6) = 3
+            // for 'b' min(1 + 1, 1) = 1
+            // result = max(3, 1) = 3
+            max = charGroups.Select(s => Math.Min(s.l + 1, charCounts[s.c])).Max();            
+
+            // case when two same char groups are splited by one char group
+            // aaabaaa -> min(3 + 1 + 3, 6) = 6
+            for(int i = 1; i < charGroups.Count - 1; i++)
+            {
+                if(charGroups[i].l == 1 && charGroups[i - 1].c == charGroups[i + 1].c)
+                    max = Math.Max(max, Math.Min(charGroups[i - 1].l + charGroups[i + 1].l + 1, charCounts[charGroups[i + 1].c]));
+            }
+
+            return max;
+        }
+
     }
 }
